147076548b9b1069aa390195c4994b69cb124505
yuyongyu <yuyongyu@tiantonglaw.com>
2018-10-23 16:17:52 +0800

    modify innerHTML

---
+++ DOM/getElementBy/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/question', function (req, res) {
+    res.sendFile(__dirname + '/现象及原因.html');
+});
+
+var server = app.listen(8000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
--- DOM/innerHTML/routes/index.js
+++ DOM/innerHTML/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/iframe-1', function(req, res, next) {
+    res.render('iframe-1');
+});
+
+router.get('/iframe-2', function(req, res, next) {
+    res.render('iframe-2');
+});
+
+router.get('/iframe-3', function(req, res, next) {
+    res.render('iframe-3');
 });
 
 module.exports = router;
