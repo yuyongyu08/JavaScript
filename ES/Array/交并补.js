7faeae1912de2af450e19a174da1d83c5d46262d
yuyongyu <yuyongyu@tiantonglaw.com>
2019-10-08 10:27:38 +0800

    modify Array

---
+++ ES/Array/交并补.js
@@ -0,0 +1,17 @@
+let arr1 = [1, 2, 3];
+let arr2 = [3, 4, 5];
+
+let intersect, union, dif;
+
+//交
+intersect = arr1.filter(item => arr2.includes(item));
+console.log(intersect);
+
+//并
+union = [...new Set([...arr1, ...arr2])];
+union = [...new Set(arr1.concat(arr2))];
+console.log(union);
+
+//补
+dif = [...arr1.filter(item => !arr2.includes(item)), ...arr2.filter(item => !arr1.includes(item))];
+console.log(dif);
