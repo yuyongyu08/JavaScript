5ca12ac8132d511a7af807cb7922a20e62691a7b
yuyongyu <yuyongyu@58.com>
2020-08-26 00:46:58 +0800

    add db

--- topics/hash-salt/test.js
+++ topics/hash-salt/test.js
@@ -1,9 +1,20 @@
 let { getSalt, getHash, checkSaltHash } = require('./index')
+let { dbConnect } = require('./db')
 
-let password = 'MYPASSWORD'
-let salt = getSalt()
-let hash = getHash(password, salt)
+// let password = 'MYPASSWORD'
+// let salt = getSalt()
+// let hash = getHash(password, salt)
 
+// console.log(checkSaltHash(password, salt, hash));
 
+ dbConnect(async client => {
+    const database = client.db("admin-mvp");
+    const collection = database.collection("users");
+    // create a document to be inserted
+    const doc = { name: "yyy", password: "123" };
+    const result = await collection.insertOne(doc);
 
-console.log(checkSaltHash(password, salt, hash));
+    console.log(
+        `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+})
