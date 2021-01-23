0ae62f39ccecd922c7a779c46637399cd126768a
yuyongyu <yuyongyu@58.com>
2021-01-23 16:24:22 +0800

    add hash

--- BOM/history/routes/index.js
+++ BOM/history/routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
+router.get('/testState', function(req, res, next) {
   res.render('pushState');
 });
+
+router.get('/testHash', function(req, res, next) {
+    res.render('hash');
+  });
 
 
 module.exports = router;
